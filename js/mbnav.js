c83981d705fbadadfd0051ef2403d942e70964e1
LSYbuilds <builddav2009@gmail.com>
2023-05-24 14:40:27 +0900

    feat : CSS,HTML 수정

---
+++ js/mbnav.js
@@ -0,0 +1,19 @@
+// // 모바일 버튼 기능
+// let mbNav = document.querySelector(".mb-nav");
+// let mbWrap = document.querySelector(".mb-wrap");
+// mbNav.addEventListener("click", function () {
+//   // 토글도 좋다.
+//   // mbNa.classList.toggle("mb-nav-active")
+//   // mb-nav-active 클래스 적용 여부(true, false)
+//   let checkActive = mbNav.classList.contains("mb-nav-active");
+
+//   if (checkActive === false) {
+//     mbNav.classList.add("mb-nav-active");
+//     mbWrap.classList.add("mb-wrap-active");
+//     // mbWrap.classList.add("mb-wrap-active");
+//   } else {
+//     mbNav.classList.remove("mb-nav-active");
+//     mbWrap.classList.remove("mb-wrap-active");
+//     // mbWrap.classList.remove("mb-wrap-active");
+//   }
+// });
