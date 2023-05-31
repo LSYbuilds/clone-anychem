131e8f135eec067d8a9a98f96155e6d449d3e7b2
LSYbuilds <builddav2009@gmail.com>
2023-05-31 18:10:22 +0900

    feat : 굿즈 슬라이드 영역 작업 진행중

--- js/visual.js
+++ js/visual.js
@@ -29,5 +29,19 @@ window.addEventListener("load", function (event) {
                 clickable: true,
             },
         });
+        // 위의 구문을 통해서 slide 가 완료되면
+        // .sw-visual-pg에는 span이 만들어 졌을 것
+        // .sw-visual-pg 밑에는 siwper-pagination-buller 이 생성됨
+        // innerHTML을 이용해서 내용을 넣어보자.
+        const swVisualBullets = document.querySelectorAll(".sw-visual-pg .swiper-pagination-bullet");
+        swVisualBullets.forEach((item, index, arr) => {
+            if (index > 9) {
+                item.innerHTML = `<em>0${index + 1}</em>`;
+            } else {
+                item.innerHTML = `<em>${index + 1}</em>`;
+            }
+
+            // item.innerHTML = `<em>${index > 10 ? `0${index + 1}` : index}</em>`;
+        });
     }
 });
