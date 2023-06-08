a1213c90d13eddbad43aac8f24d8ff965569ecb8
LSYbuilds <builddav2009@gmail.com>
2023-06-08 18:09:01 +0900

    feat : news 데이터 작업진행중

---
+++ js/news.js
@@ -0,0 +1,38 @@
+window.addEventListener("load", function () {
+    // 외부 데이터 로딩
+    //   fetch("data/newsdata.json")
+    //     .then((res) => res.json())
+    //     .then((result) => console.log(result))
+    //     .catch((err) => console.log(err));
+    const getNews = async () => {
+        try {
+            const res = await fetch("data/newsdata.json");
+            const result = await res.json();
+            console.log(result);
+            makeHtml(result);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    let makeHtml = (_data) => {
+        let html = ``;
+        _data.forEach((item, index) => {
+            let tag = `
+            <div class="swiper-slide">
+                <div class="news-box">
+                        <a href="${item.link}">
+                            <span>${item.cate}</span>
+                            <p>${item.title}</p>
+                            <span><i class="fa-regular fa-clock"></i>${item.date}</span>
+                        </a>
+                </div>
+            </div>
+            `;
+            html += tag;
+        });
+        console.log(html);
+    };
+
+    getNews();
+});
